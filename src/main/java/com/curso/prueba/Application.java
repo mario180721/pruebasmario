23d9bfe7b06db80161c52a4d80db32cfc1ee53eb
Mario180721 <63652719+lucia8716@users.noreply.github.com>
2021-09-21 16:01:10 +0200

    v

---
+++ src/main/java/com/curso/prueba/Application.java
@@ -0,0 +1,15 @@
+package com.curso.prueba;
+
+import org.apache.log4j.Logger;
+
+public class Application
+
+{
+
+	static Logger log=Logger.getLogger(Application.class);
+	
+	public static void main( String[] args )
+	{
+		log.debug("cerebro de mono");
+	}
+}
